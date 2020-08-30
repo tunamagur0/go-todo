b729463d98703c36a826b43592546b3e3d8ae3ea
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-08-30 22:38:34 +0900

    Change db field done -> status

--- backend/src/models/todo.go
+++ backend/src/models/todo.go
@@ -2,10 +2,17 @@ package models
 
 import "time"
 
+const (
+	StatusNew = iota
+	StatusWIP
+	StatusDone
+	StatusPending
+)
+
 type Todo struct {
 	ID         string     `json:"id" gorm:"primary_key;type:text"`
 	Content    string     `json:"content" gorm:"type:text"`
-	Done       bool       `json:"done"`
+	Status     int        `json:"status"`
 	CreatedAt  time.Time  `json:"created_at"`
 	UpdatedAt  time.Time  `json:"-"`
 	FinishedAt *time.Time `json:"finished_at"`
